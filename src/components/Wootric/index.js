b8b86bab395e54ce981f7abeb6a8a8094cfee4cb
Nailson <nailson.ivs@codens.com.br>
2020-03-11 19:00:36 -0300

    update nps

---
+++ src/components/Wootric/index.js
@@ -0,0 +1,47 @@
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from "react-redux";
+
+const Wootric = () => {
+  const current_user = useSelector(state => state.auth);
+
+  useEffect(() => {
+    async function Startwootric () {
+      console.log(current_user)
+      let setupScript = document.createElement('script');
+      setupScript.type = 'text/javascript';
+      setupScript.async = true;
+      setupScript.innerHTML = `
+        wootric_no_surveyed_cookie = true;
+        wootric_survey_immediately = true;
+        window.wootricSettings = {
+          email: '${current_user.email}',
+          created_at: 1234567890,
+          account_token: 'NPS-d2b930e4'
+        };
+      `;
+      document.body.appendChild(setupScript);
+
+      // Beacon
+      let beacon = document.createElement('script');
+      beacon.type = 'text/javascript';
+      beacon.async = true;
+  
+      beacon.src = 'https://cdn.wootric.com/wootric-sdk.js';
+      beacon.onload = function() {
+        window.wootric('run');
+      };
+      document.body.appendChild(beacon);
+    }
+    Startwootric()
+
+    return () => {
+    };
+  }, [])
+
+  return (
+    <>
+    </>
+  )
+}
+
+export default Wootric;
